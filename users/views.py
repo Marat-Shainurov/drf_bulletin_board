17fcd63ffa03b9e48ade02b9571b14a2155587aa
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-18 13:03:41 +0300

    overrides get_permissions for users

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from users.models import CustomUser
 from users.serializers import CustomUserCreateSerializer, CustomerUserBaseSerializer, CustomUserUpdateSerializer
@@ -16,3 +17,10 @@ class CustomerUserViewSet(viewsets.ModelViewSet):
             return CustomUserUpdateSerializer
         else:
             return CustomerUserBaseSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
